8bab705636e1ad3e32cfff5feb4747ded3c8820a
Prasad-maker <65820623+Prasad-maker@users.noreply.github.com>
2026-06-23 15:52:18 +0530

    Add: course-schedule-iv - submission-1

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = defaultdict(list)
+        for u,v in prerequisites:
+            adj[v].append(u)
+        prereqmap = {}
+        def dfs(crs):
+            if crs not in prereqmap:
+                prereqmap[crs] = set()
+                for prereq in adj[crs]:
+                    prereqmap[crs] |= dfs(prereq)
+                prereqmap[crs].add(crs)
+            return prereqmap[crs]
+        res = []
+        for u,v in queries:
+            dfs(v)
+            res.append(u in prereqmap[v])
+        return res
+        
+            
+        
