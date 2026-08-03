d2c417a78d122090e71684c5a76f12a45552097a
Prasad-maker <65820623+Prasad-maker@users.noreply.github.com>
2026-08-03 23:10:21 +0530

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = [0]*26
+        res = [""]*len(s)
+        total = 0
+        for c in s:
+            count[ord(c)-ord('a')]+= 1
+        heap = []
+        for i in range(26):
+            heap.append((-count[i],chr(ord('a')+i)))
+        heapq.heapify(heap)
+        cur = 0
+        odd = False
+        while total<len(s):
+
+            count,char = heapq.heappop(heap)
+            count = -count
+            print(count,char)
+            if count>(len(s)+1)//2:
+                return ""
+            for i in range(count):
+                res[cur]=char
+                cur+=2
+                if cur>=len(s):
+                    cur=1
+            total+=count
+
+        return "".join(res)
+        
+        
+        
