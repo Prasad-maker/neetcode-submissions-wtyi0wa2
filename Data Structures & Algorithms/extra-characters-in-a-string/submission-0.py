90e1756aac29620ff6edcae1371b4db1e8b1d92c
Prasad Jadhav <65820623+Prasad-maker@users.noreply.github.com>
2026-09-04 22:26:39 +0530

    Add: extra-characters-in-a-string - submission-0

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n = len(s)
+        dp = [float("inf")]*(n+1)
+        dp[n] = 0
+        for i in range(n-1,-1,-1):
+            dp[i] = 1+dp[i+1]
+            for word in dictionary:
+                if i+len(word)<=n and s[i:i+len(word)] == word:
+                    dp[i] = min(dp[i],dp[i+len(word)])
+        return dp[0]
